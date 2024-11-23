8e862800b0507893652fe3768ee11d254a932f04
giorgiabes <abesadze.george@gmail.com>
2024-11-23 18:45:44 +0400

    add game loop

--- src/main.c
+++ src/main.c
@@ -1,1 +1,83 @@
 #include <SDL2/SDL.h>
+
+// #include "../lib/lua/src/lua.h"
+// #include "../lib/lua/src/lualib.h"
+// #include "../lib/lua/src/lauxlib.h"
+
+
+#define FALSE 0
+#define TRUE 1
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+int is_running = FALSE;
+
+int initialize_window(void) {
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		fprintf(stderr, "Error initializing SDL.\n");
+		return FALSE;
+	}
+	window = SDL_CreateWindow(
+		NULL,
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		WINDOW_WIDTH,
+		WINDOW_HEIGHT,
+		SDL_WINDOW_BORDERLESS
+	);
+	if (!window) {
+		fprintf(stderr, "Error creating SDL window.\n");
+		return FALSE;
+	}
+	renderer = SDL_CreateRenderer(window, -1, 0);
+	if (!renderer) {
+		fprintf(stderr, "Error creating SDL renderer.\n");
+		return FALSE;
+	}
+	return TRUE;
+}
+
+void process_input(void) {
+	SDL_Event event;
+	SDL_PollEvent(&event);
+	switch (event.type) {
+		case SDL_QUIT: {
+			is_running = FALSE;
+			break;
+		}
+		case SDL_KEYDOWN: {
+			if (event.key.keysym.sym == SDLK_ESCAPE)
+				is_running = FALSE;
+			break;
+		}
+	}
+}
+
+void update(void) {
+	// TODO:
+}
+
+void render(void) {
+	// TODO:
+}
+
+void destroy_window(void) {
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
+
+int main(int argc, char *argv[]) {
+	is_running = initialize_window();
+
+	while (is_running) {
+		process_input();
+		update();
+		render();
+	}
+
+	destroy_window();
+	return 0;
+}
